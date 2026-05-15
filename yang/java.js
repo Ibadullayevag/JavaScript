7cd61471ac021ef57319b998b8f637299b72d542
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-05-15 17:59:24 +0500

    add new files

---
+++ yang/java.js
@@ -0,0 +1,86 @@
+
+//!^1.Voyaga yetgan va yetmaganligini aniqlash//
+
+// let yosh = 12
+// if (yosh > 17) {
+//     console.log("Siz voyaga yetgansiz")
+// } else {
+//     console.log("Siz voyaga yetmagansiz")
+// }
+
+
+
+// let son = 12
+// if (son % 2 === 0) {
+//     console.log("Siz juft son kiritdingiz")
+// } else {
+//     console.log("Siz toq son kiritdingiz")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//^ Topshiriq
+
+// let sinf = 6
+// if (sinf > 11) {
+//     console.log("Siz maktabni tugatgansiz")
+// } else{
+//     console.log("Siz hali maktabda o'qiysiz")
+// }
+
+// ///
+
+// let gradus = 20
+// if (gradus > 30) {
+//     console.log("Bugun havo juda issiq")
+// } else {
+//     console.log("Bugun havo juda issiq emas")
+// }
+
+
+
+// //^ Musbat va manfiy son 0
+
+// let son = 1
+// if(son > 0) {
+//     console.log("Siz musbat son kiritdingiz")
+// }
+// else if (son === 0) {
+//     console.log("Siz 0 kiritdingiz")
+// } else {
+//     console.log("Siz manfiy son kiritdingiz")
+// } 
+
+//^
+let parol = "qweqwe"
+if (parol =="qweqwe") {
+    console.log("Tizim ochildi")
+}
+else {
+    console.log("Parol xato")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
