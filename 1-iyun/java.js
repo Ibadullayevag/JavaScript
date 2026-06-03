8f5f7fd5bb5d192b52e88c52914e750da48df1c1
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-06-03 14:15:45 +0500

    +

---
+++ 1-iyun/java.js
@@ -0,0 +1,82 @@
+
+// for (let boshlanishi = 1; boshlanishi <=10; boshlanishi++){
+//     console.log(boshlanishi)
+// }
+
+// for (let boshlanishi = 3; boshlanishi <=23; boshlanishi++){
+//     console.log(boshlanishi)
+// }
+
+
+// for(let i=0; i<101; i++){
+    
+//     if(i%2===0){
+//         console.log(i)
+//     }
+// }
+
+// for (let i=0; i<101; i++){
+//     if(i%2 !==0){
+
+//         console.log( i)
+//     }
+
+
+
+
+// }
+
+
+// for(let i=0; i<101; i++){
+
+//     if(i%3 ===0 && i%5 ===0){
+//         console.log(i)
+//     }
+//     }
+
+
+
+// for(let i=0; i=101; i++){
+//     if(i%3 ===0 && i%4 ===0){
+//         console.log(i)
+//     }
+// }    
+
+
+
+
+
+
+
+
+
+
+
+
+//^Uyga Vazifa
+//*1. 0dan 1000gacha bo'lsan sonlarning kvadratini chiqaring
+//*2. 0dan 1000gacha bo'lgan sonlar orasidan 3 va 4ga bo'linadigan sonlarning kubini chiqaring
+//*3. 0dan 1000gacha bo'lgan sonlarning 5ga bo'linadiganlar orasidan 2ga qoldiqsiz bo'linadiganin chiqaring
+
+//^ Birinchi
+
+// for (let i=0; i<=100; i++) {
+
+//     console.log(i * i)
+// }
+
+//^Ikkinchi
+
+// for (let i=0; i<=1000; i++){
+
+//     if(i%3 ===0 && i%4 ===0){
+//         console.log(i **3)  }
+// }
+
+//^Uchinchi
+
+// for (i=0; i<=1000;i++){
+//     if(i % 5===0 && i%2 ===0){
+//         console.log(i)
+//     }
+// }
