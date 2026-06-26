3b4bb2ba776f190c024a9cf3986cc518282e0fad
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-06-26 17:07:59 +0500

    console

---
+++ 26-iyun/main.js
@@ -0,0 +1,30 @@
+let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54];
+
+// arr. push (1, 2,3,4, 5)
+
+// arr.unshift(1,2,3,4,5)
+
+// arr.shift()
+
+// arr. splice(1, 15, 7);
+
+// arr. splice(1, 15);
+
+// arr.splice(1);
+
+// let indeksiniQaytarish = arr. index0f(54)
+
+// let indeksiniQaytarishl = arr.index0f(545)
+
+
+
+// console.Log(indeksini@aytarish, indeksiniQaytarish1)
+
+// Let massiv = arr. LastIndex0f(54)
+
+// console. Log (massiv)
+
+// let findMetod = arr.find(i => i > 300)
+//  console.log(findMetod)
+
+//  console.log(arr)
