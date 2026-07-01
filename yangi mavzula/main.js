fd558c6bdcc2dcc9b7a4152b028f2d859434a1e4
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-07-01 17:50:30 +0500

    add new files

---
+++ yangi mavzula/main.js
@@ -0,0 +1,9 @@
+
+let arr = [1, 2, [3, 4, 5, [6, 7], [8, 9]], 10, 11, [12, [13, [14, [15, 16]], 17, [18, 19], 20], 21], 23];
+
+//* 15, 17, 11, 19, 20, 21, 23
+
+// let yigindisi = arr.reduce((yigiladi, iterator) => yigiladi + iterator)
+// console.log(yigindisi)
+console.log( arr [5][1][1][1][0])
+console.log( arr [5][1][3][1])
