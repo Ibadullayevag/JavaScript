0809b5ce4891d64e2f479849cf7ca778128efc83
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-07-17 17:24:06 +0500

    add

---
+++ function/function.js
@@ -0,0 +1,17 @@
+//* Function Declaration
+function ikkiSonVigindisi(a, b, c) {
+console. log(a + b + c);}
+ikkiSonYigindisi(2, 4, 40);
+function ayirma(a = 1, b = 2, c = 3) {
+console.log(a - b - c)
+ayirma ( )
+}
+
+//* Function Expression
+const ikkiSonYig = function (a, b, c) {
+console. log(a + b + c);}
+ikkiSonYig(1, 2, 3);
+
+//* Arrow Function
+const ayirmaa = (a, b, c) => a - b - c;
+console.log(ayirmaa (1, 2, 3))
