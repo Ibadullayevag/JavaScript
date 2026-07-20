6dcd7037fdc59653785d896a217dc014199549ad
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-07-20 17:25:52 +0500

    add

---
+++ function/object.js
@@ -0,0 +1,17 @@
+const obyekt = {
+    ism: "Shoxruzbek",
+    kasbi: "bekorchi",
+    arr: [1,2,3,4,5,6],
+    suhrob: function () {
+          for(let i of this.arr){
+              if (i % 2 ==0 ) {
+                console.log(i)
+            }
+        }
+    },
+    arrowFunksiya: () => {
+        console.log(this.ism);
+    }
+}
+
+obyekt.suhrob(),
