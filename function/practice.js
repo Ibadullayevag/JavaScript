93cf61a41a80ec8c13996ed5124ed37e4fb8a1df
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-07-26 22:11:32 +0500

    add

--- function/practice.js
+++ function/practice.js
@@ -169,18 +169,18 @@ function masala18(massiv) {}
 
 // 19-masala: [if-else, for] Massivdagi juft sonlarni alohida
 // massivga ajratuvchi funksiya yozing.
-function masala19(massiv) {
-  let juftla = [];
+// function masala19(massiv) {
+//   let juftla = [];
 
-  for (let i=0; i < massiv.length; i++) {
-    if (massiv[i] % 2===0) {
-      juftla.push(massiv[i]);
-    }
-  }
+//   for (let i=0; i <=10; i++) {
+//     if (massiv[i] % 2===0) {
+//       juftla.push(massiv[i]);
+//     }
+//   }
 
-  return juftla;
-}
-console.log(masala19([1, 2, 3, 4, 5, 6,7,8,9,10]));
+//   return juftla;
+// }
+// console.log(masala19([1, 2, 3, 4, 5, 6,7,8,9,10]));
 
 // 20-masala: [if-else, for] Massivdagi toq sonlarni alohida
 // massivga ajratuvchi funksiya yozing.
