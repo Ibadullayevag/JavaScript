a31ecb0dc3d5acc7a4c9c2171b165e8911f5248d
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-08-05 17:16:00 +0500

    add

--- built/topshiriqlar.js
+++ built/topshiriqlar.js
@@ -20,5 +20,33 @@
 // console.log(hozirgiKUn) 
 
 //5-topshiriq Hozirgi sanani ISO formatda (YYYY-MM-DDTHH:mm:ss.sssZ) konsolga chiqaruvchi dastur yozing.
-const sana = new Date ()
-console.log(sana.toISOString)
+// const sana = new Date ()
+// console.log(sana.toISOString)
+
+//6-topshiriq 1970-yil 1-yanvardan hozirgi vaqtgacha bo'lgan millisekundlar sonini konsolga chiqaruvchi dastur yozing.
+// console.log(Date.now())
+
+//7-topshiriq Hozirgi sanani o'zbek tilida ko'rsatuvchi dastur yozing. (Intl.DateTimeFormat dan foydalaning)
+
+
+//8-topshiriq Hozirgi vaqtga 5 soat qo'shib, yangi vaqtni konsolga chiqaruvchi dastur yozing.
+// const vaqt = new Date().getHours() +5 ;
+// console.log(vaqt)
+
+
+
+//^Math Object (9-19 topshiriqlar)
+
+//9-topshiriq Math.PI yordamida Pi sonini konsolga chiqaring.
+// console.log(Math.PI)
+
+//10-topshiriq 0 va 1 oralig'ida tasodifiy son hosil qiling va konsolga chiqaring.
+function tasodifiySon(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(tasodifiySon(1, 6))
+
+//11-topshiriq 4.7 sonini eng yaqin butun songa yaxlitlang va natijani konsolga chiqaring.
+
+//12-topshiriq 4.9 sonini pastga (floor) yaxlitlang va natijani konsolga chiqaring.
