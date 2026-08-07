6b81321467e64ede3d92233e15a931727def6663
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-08-07 17:27:51 +0500

    ad

---
+++ D.O.M/main.js
@@ -0,0 +1,9 @@
+const jsbox = document.getElementById(`box`)
+console.log(jsbox)
+
+jsbox.style.backgroundColor = "blue"
+jsbox.style.width = "500px"
+jsbox.style.height = "500px"
+jsbox.style.borderRadius = "50%"
+
+
