9434f923adc4aa6dd95dab346a9d64ddd43d84ec
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-08-10 17:09:11 +0500

    ad

--- D.O.M/main.js
+++ D.O.M/main.js
@@ -1,9 +1,63 @@
-const jsbox = document.getElementById(`box`)
-console.log(jsbox)
+// const jsbox = document.getElementById(`box`)
+// console.log(jsbox)
 
-jsbox.style.backgroundColor = "blue"
-jsbox.style.width = "500px"
-jsbox.style.height = "500px"
-jsbox.style.borderRadius = "50%"
+// jsbox.style.backgroundColor = "blue"
+// jsbox.style.width = "500px"
+// jsbox.style.height = "500px"
+// jsbox.style.borderRadius = "50%"
 
 
+const main = document.querySelector(".main");
+const box = document.querySelector(".box")
+const box1 = document.querySelector(".box1")
+const box2 = document.querySelector(".box2")
+const box3 = document.querySelector(".box3")
+const box4 = document.querySelector(".box4")
+const box5 = document.querySelector(".box5")
+
+
+///
+
+main.style.width = "900px"
+main.style.height = "600px"
+main.style.border = "2px solid red"
+main.style.display ="flex"
+main.style.justifyContent ="center"
+main.style.alignItems = "center"
+
+document.body.style.display = "flex"
+document.body.style.justifyContent ="center"
+main.style.flexWrap = "wrap";  
+main.style.gap = "200px";
+main.style.padding = "50px"
+console.log(main)
+
+///
+box.style.width ="150px"
+box.style.height ="150px"
+box.style.backgroundColor = "red"
+
+//
+box1.style.width ="150px"
+box1.style.height ="150px"
+box1.style.backgroundColor = "green"
+
+///
+box2.style.width ="150px"
+box2.style.height ="150px"
+box2.style.backgroundColor = "yellow"
+
+///
+box3.style.width ="150px"
+box3.style.height ="150px"
+box3.style.backgroundColor = "pink"
+
+///
+box4.style.width ="150px"
+box4.style.height ="150px"
+box4.style.backgroundColor = "blue"
+
+///
+box5.style.width ="150px"
+box5.style.height ="150px"
+box5.style.backgroundColor = "brown"
