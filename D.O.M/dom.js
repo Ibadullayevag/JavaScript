845829509da5262b2666ef2aa8b9b6ad20ae0a75
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-08-10 17:37:27 +0500

    as

---
+++ D.O.M/dom.js
@@ -0,0 +1,42 @@
+// const main = document.querySelector(".main");
+
+// const h11 = document.querySelector(".h1")
+// const p = document.createElement("p")
+// p.textContent = "Shoxruz"
+// document.body.append(p)
+// p.style.color = "red"
+// p.className = "p1"
+// p.id = "p1"
+
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "Bu h1 da yozilgan text"
+// document.body.append(h1)
+// h1.style.color ="blue"
+// h1.className = "h1"
+// h1.id = "h1"
+
+// main.style.width = "900px"
+// main.style.height = "600px"
+
+// main.style.display ="flex"
+// main.style.justifyContent ="center"
+// main.style.alignItems = "center"
+// main.style.backgroundColor = "pink"
+
+// document.body.style.display = "flex"
+// document.body.style.justifyContent ="center"
+
+
+
+// h11.style.textAlign = "center"
+
+
+
+
+
+
+
+
+
+////////////
