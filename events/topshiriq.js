38df0b9c04ec4c80f0c06f6ec342260e84274f1e
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-08-17 16:50:09 +0500

    asd

--- events/topshiriq.js
+++ events/topshiriq.js
@@ -1,5 +1,13 @@
+const qizilchiroq = document.querySelector(".qizil-chiroq");
+const sariqchiroq = document.querySelector(".sariq-chiroq");
+const yashilchiroq = document.querySelector(".yashil-chiroq");
 
-const qizilchiroq = document.querySelector("qizil-chiroq")
-const sariqchiroq = document.querySelector("sariq-chiroq")
-const yashilchiroq = document.querySelector("yashil-chiroq")
+const ovoz = document.querySelector("#qizil-audio");
 
+qizilchiroq.onclick = function () {
+    qizilchiroq.style.width = "210px";
+    qizilchiroq.style.height = "210px";
+
+    ovoz.currentTime = 0;
+    ovoz.play();
+};
