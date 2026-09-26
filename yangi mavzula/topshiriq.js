3b1b4790ce5ffc969f8ab2c8944bb91c6c4c28f1
Ibadullayevag <ibadullayevaguncha@gmail.com>
2026-09-26 21:31:00 +0500

    add

--- yangi mavzula/topshiriq.js
+++ yangi mavzula/topshiriq.js
@@ -557,8 +557,8 @@
 // 84-topshiriq (splice)
 // let arr = [1, 2, 3, 4, 5];
 // splice() yordamida massivning barcha elementlarini o'chirib, o'rniga [9, 9, 9] massivini joylang.
-let arr = [1, 2, 3, 4, 5]
-arr.splice = ([9,9,9])
+// let arr = [1, 2, 3, 4, 5]
+// arr.splice = ([9,9,9])
 
 
 // 85-topshiriq (keys)
